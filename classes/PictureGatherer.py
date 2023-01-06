c9a5fc2460b62be04d2b0e1c1608fadf4dd82523
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-01-06 22:41:11 +0100

    made class that download pictures based of a word

---
+++ classes/PictureGatherer.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+class PictureGatherer:
+    def __init__(self, amountOfPictures, *args):
+        self.folder = "..\pictures"
+        self.imageUrl = "https://www.google.com/search?q={}&tbm=isch&start={}"
+        self.imagesPerPage = 20
+        self.amountOfPictures = amountOfPictures
+        self.words = args
+    def run(self):
+        for word in self.words:
+            self.prepareImages(word, self.amountOfPictures)
+
+
+    def downloadImages(self, word, urlArray):
+        if not os.path.exists(self.folder):
+            os.mkdir(self.folder)
+
+        if not os.path.exists(f"{self.folder}\\{word}"):
+            os.mkdir(f"{self.folder}\\{word}")
+
+        print("[*] Downloading pictures")
+        for i, url in enumerate(urlArray):
+            r = requests.get(url)
+            if r.status_code == 200:
+                with open(f"{self.folder}/{word}/{word}_{i}.jpg", "wb") as f:
+                    f.write(r.content)
+            print(f"[*] Downloaded picture {i+1}/{self.amountOfPictures}")
+
+    def prepareImages(self,word, amount):
+
+        print(f"[*] Finding images of {word}")
+
+        currentTotalImages = 0
+        imagesUrlArray = []
+        while currentTotalImages < amount:
+
+            amountToGet = min(amount - currentTotalImages, self.imagesPerPage)
+
+            curUrl = self.imageUrl.format(word, currentTotalImages)
+            currentUrls = self.getImagesUrls(curUrl, amountToGet)
+
+            imagesUrlArray += currentUrls
+
+            currentTotalImages += self.imagesPerPage
+
+        self.downloadImages(word, imagesUrlArray)
+
+    def getImagesUrls(self, url, amount=None):
+        if not amount or amount > self.imagesPerPage:
+            amount = self.imagesPerPage
+
+        r = requests.get(url)
+        html = r.text
+        soup = BeautifulSoup(html, "html.parser")
+        tableOfImages = soup.find("table", {"class":"GpQGbf"})
+        imagesTags = tableOfImages.find_all("img")
+
+        allImageUrls = []
+
+        for i in range(amount):
+            img = imagesTags[i]
+            allImageUrls.append(img.get("src"))
+
+        return allImageUrls
+
+p = PictureGatherer(10, "dogs", "cats")
+
+p.run()
